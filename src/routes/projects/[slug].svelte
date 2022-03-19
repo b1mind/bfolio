f3fb636996dabafd5f8962af50aaa7d2d2315374
b1mind <brent@1mindsolutions.com>
2022-03-19 14:12:36 -0600

    🧹 handle dynamic data

--- src/routes/projects/[slug].svelte
+++ src/routes/projects/[slug].svelte
@@ -3,9 +3,6 @@
   import { projectsStore } from '$lib/data/projects.js'
 
   $: currentProject = $projectsStore[$page.params.slug]
-  console.log(Object.keys($projectsStore))
-
-  console.log($projectsStore[$page.params.slug])
 </script>
 
 <!-- //todo think about the injection, 
@@ -13,33 +10,30 @@ how is the data from the backend returned? does markup get injected? -->
 <article>
   <header>
     <h2>{currentProject.name}</h2>
-    <i>An interactive way to order a pizza</i>
+    <i>{currentProject.subTitle}</i>
   </header>
 
-  <section>
-    <h3>Overview</h3>
-    <img src="https://picsum.photos/800/600?random=" alt="" />
+  {#each currentProject.sections as section}
+    <section>
+      <h3>{section.title}</h3>
+      <img src={section.img} alt={section.altText} />
 
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente explicabo,
-      architecto temporibus aspernatur repellendus saepe vel laborum voluptates molestias
-      rerum ex repellat quas error id ipsa unde nisi optio ad!
-    </p>
-  </section>
-
-  <section>
-    <h3>Challenges</h3>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente explicabo,
-      architecto temporibus aspernatur repellendus saepe vel laborum voluptates molestias
-      rerum ex repellat quas error id ipsa unde nisi optio ad!
-    </p>
-  </section>
+      <p>{section.content}</p>
+    </section>
+  {/each}
 </article>
 
 <style lang="scss">
   h2,
   h3 {
     margin: 0;
+  }
+
+  img {
+    // max-width: 280px;
+    width: max(280px, 100%);
+    max-height: 300px;
+    object-fit: cover;
+    z-index: -99;
   }
 </style>
