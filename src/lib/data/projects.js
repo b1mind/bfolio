e8382bef9d1e2b6d51b38cf3b8cf9d865d11199e
b1mind <brent@1mindsolutions.com>
2022-05-10 15:40:41 -0600

    ➕update: added learned

--- src/lib/data/projects.js
+++ src/lib/data/projects.js
@@ -12,7 +12,7 @@ const data = {
     overview: {
       title: 'Overview',
       content:
-        '<p>Always evolving and creating different OBS browser layers and scenes that interact with Twitch. <p>Animations using a mix of CSS, Svelte, and GreenSock. Mostly designed and developed while live streaming and coding in public.</p>',
+        '<p>Always evolving and creating different OBS browser layers and scenes that interact with Twitch.</p> <p>Animations using a mix of CSS, Svelte, and GreenSock. Mostly designed and developed while live streaming and coding in public.</p>',
       img: '/img/overlay/overlay-overview.jpg',
       altText: 'img alt text',
     },
@@ -23,8 +23,8 @@ const data = {
       {
         title: 'Alerts',
         content:
-          '<p>Working with Twitch api and learning how to test and mimic alerts. Used a wrapper library ComfyJS to interface with api easier. Then using Svelte stores for state management and animation of the alerts queue.</p>',
-        img: '/img/overlay/overlay-overview.jpg',
+          '<p>Working with Twitch api and learning how to test and mimic alerts. Used a wrapper library <a href="#">ComfyJS</a> to interface with api easier. Then using Svelte stores for state management and animation of the alerts queue.</p>',
+        img: 'https://picsum.photos/300/300?random=1',
         altText: 'img alt text',
       },
 
@@ -44,6 +44,12 @@ const data = {
         altText: 'Animations example',
       },
     ],
+
+    learned: {
+      title: 'Learned',
+      content:
+        '<p>How to use the new routing and layouts in SvelteKit. Using data-sets for dynamic styling components based on the value of the data. Passing url paramaters as props to get data from a JSON or request response.</p>',
+    },
   },
 
   knittingpro: {
@@ -57,7 +63,7 @@ const data = {
     overview: {
       title: 'Overview',
       content:
-        '<p>Knitting Pro course website is a mock website designed by Kevin Powell and given to students as a challenge in his Demystified CSS paid course. Meant to be a CSS class project I took the chance to use SvelteKit and learn how I would handle CSS utility classes and styled components.</p>',
+        '<p>Knitting Pro course website is a mock website designed by Kevin Powell and given to students as a challenge in his Demystified CSS paid course.</p> <p> Meant to be a CSS class project I took the chance to use SvelteKit and learn how I would handle CSS utility classes and styled components.</p>',
       img: '/img/knitting/knitting-overview.jpg',
       altText: 'Knitting Pro Website',
     },
@@ -77,7 +83,7 @@ const data = {
         title: 'Responsive',
         content:
           '<p>The goal of the challenge was to code from a design that provided three resolution to follow. Creating responsive breakpoints and utility classes to meet specifications provided.</p>',
-        img: 'https://picsum.photos/300/300?random=4',
+        img: 'https://picsum.photos/300/300?random=9',
         altText: 'Show responsive website',
       },
 
@@ -89,6 +95,12 @@ const data = {
         altText: 'Showing components',
       },
     ],
+
+    learned: {
+      title: 'Learned',
+      content:
+        '<p>Streaming hardware and software. The in and outs of the OBS application and how Twitch works. Using different browsers and web pages to overlay and create different scenes. How to code in front of others.</p>',
+    },
   },
 
   pizzaapp: {
@@ -102,7 +114,7 @@ const data = {
     overview: {
       title: 'Overview',
       content:
-        '<p>Wanted to make a fun application to learn different things. What better way than making a pizza (think python make pizza example).</p>',
+        '<p>Wanted to make a fun application to learn different things.</p> <p>What better way than making a pizza (think python make pizza example).</p>',
       img: '/img/pizza-app/pizza-overview.jpg',
       altText: 'pizza app with draggable toppings',
     },
@@ -114,7 +126,7 @@ const data = {
         title: 'Assets',
         content:
           '<p>Using SVG sprites to limit the requests to the user. Using smart naming conventions to allow for small code and easy component state to display correct graphics.</p>',
-        img: '/img/pizza-app/pizza-main.jpg',
+        img: 'https://picsum.photos/300/300?random=0',
         altText: 'confirm pizza toppings',
       },
 
@@ -122,7 +134,7 @@ const data = {
         title: 'Interaction',
         content:
           '<p>Drag and drop animations using GreenSock and Svelte components. Allow users to drag a topping onto the pizza to add as well as click if added to remove toppings.</p>',
-        img: 'https://picsum.photos/300/300?random=5',
+        img: 'https://picsum.photos/300/300?random=1',
         altText: 'order price calculation',
       },
 
@@ -130,10 +142,16 @@ const data = {
         title: 'Calculation',
         content:
           '<p>As the user changes the size and toppings of the pizza cost will be reactively updated keeping precise track of what is ready to be ordered.</p>',
-        img: 'https://picsum.photos/300/300?random=5',
+        img: 'https://picsum.photos/300/300?random=12',
         altText: 'order price calculation',
       },
     ],
+
+    learned: {
+      title: 'Learned',
+      content:
+        '<p> SVG sprites and how to use them in components. Using data-set to help pass naming conventions along to keep every thing in sync. Wanted to try making HTML details and summary modal for oder confermation screen.</p>',
+    },
   },
 }
 
