0284b3aa082c0b6d1b4941f21ef73d5d7544e17b
b1mind <brent@1mindsolutions.com>
2022-05-11 17:37:33 -0600

    🧙‍♂️animations for landing page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,25 +1,69 @@
 <script>
+  import { onMount } from 'svelte'
+  import gsap from 'gsap'
+
   // import { page } from '$app/stores'
 
   // $: path = $page.url.pathname
+  const tl = gsap.timeline({})
+
+  onMount(() => {
+    // const links = document.querySelectorAll('.mainNav > a')
+    const links = gsap.utils.toArray('.mainNav > a')
+
+    tl.add('start')
+    tl.set('.logo > b', { opacity: 0 })
+    tl.set(links, { opacity: 0, x: 300, attr: { 'pointer-events': 'none' } })
+    tl.set('.logo', { opacity: 0, y: '20vh', scale: 5 })
+
+    tl.to('.logo', { duration: 1.5, opacity: 1 })
+    // tl.from('.logo', {}, '<')
+    tl.from(
+      '.logo-bg',
+      {
+        duration: 1.5,
+        scale: 30,
+        // delay: 0.25,
+        ease: 'sine.out',
+      },
+      '<',
+    )
+
+    tl.to('.logo', { y: 0, scale: 1, ease: 'sine.out' }, '>-0.25')
+    tl.to('.logo > b', { opacity: 1 }, '-=0.1')
+    tl.to(
+      links,
+      {
+        opacity: 1,
+        x: 0,
+        stagger: { amount: 0.3 },
+        ease: 'back.out',
+      },
+      '-=0.2',
+    )
+  })
+
+  function testAnime() {
+    tl.play(0)
+  }
 </script>
 
 <!-- //test how does a screen reader read mainNav as camelCase? reason to use spinal?-->
 <header class="wrap">
   <nav class="mainNav">
-    <div class="logo">
+    <div class="logo" on:click={testAnime}>
       <b>Bren</b>
-      <svg height="36" viewBox="0 0 125 117" xmlns="http://www.w3.org/2000/svg">
+      <svg height="36" viewBox="0 0 130 130" xmlns="http://www.w3.org/2000/svg">
         <path
-          d="M24 44.9984H9V14.9984C9.00153 18.6099 3.73701 18.0063 0 17.9984V6.99838C9.95537 7.00583 7.42383 0.453084 8.98668 0C14.8419 0 18.1509 0.0337733 24 0V44.9984Z"
-          fill="#ffffff"
+          d="M28 51.9984H13V21.9984C13.0015 25.6099 7.73701 25.0063 4 24.9984V13.9984C13.9554 14.0058 11.4238 7.45308 12.9867 7C18.8419 7 22.1509 7.03377 28 7V51.9984Z"
+          fill="white"
         />
         <path
-          d="M74 83C76.5 74 81.2187 63.1345 86.5 52.8315C92.3874 41.3165 98.6618 32.0525 104.463 25.0395C106.281 22.7884 107.836 21.5195 110 21V117H125V0.104633H110C107.403 0.104633 98.6185 6.85783 89.5276 20.3642C80.4368 33.784 73.829 45.2165 69.5 55C65.0845 45.1299 58.359 33.784 49.5279 20.624C40.3505 6.94441 34.684 0 32 0C32 12.1163 32 21 32 21C37.7142 27.9264 45.8916 41.2299 51.8655 52.8315C57.1469 63.0479 61.7966 73.3897 65 83C65.9524 85.9437 66 87 66.5 88C67 89 68 90 69.5 90C71 90 72 89 72.5147 88.0261C73.0571 87 73 86 74 83Z"
+          d="M79 90C81.5 81 86.2187 70.1345 91.5 59.8315C97.3874 48.3165 103.662 39.0525 109.463 32.0395C111.281 29.7884 112.836 28.5195 115 28V124H130V7.10463H115C112.403 7.10463 103.618 13.8578 94.5276 27.3642C85.4368 40.784 78.829 52.2165 74.5 62C70.0845 52.1299 63.359 40.784 54.5279 27.624C45.3505 13.9444 39.684 7 37 7C37 19.1163 37 28 37 28C42.7142 34.9264 50.8916 48.2299 56.8655 59.8315C62.1469 70.0479 66.7966 80.3897 70 90C70.9524 92.9437 71 94 71.5 95C72 96 73 97 74.5 97C76 97 77 96 77.5147 95.0261C78.0571 94 78 93 79 90Z"
         />
-        <path d="M24 54H9V69H24V54Z" />
-        <path d="M24 78H9V93H24V78Z" />
-        <path d="M24 102H9V117H24V102Z" />
+        <path d="M28 61H13V76H28V61Z" />
+        <path d="M28 85H13V100H28V85Z" />
+        <path d="M28 109H13V124H28V109Z" />
       </svg>
 
       <b>orton</b>
