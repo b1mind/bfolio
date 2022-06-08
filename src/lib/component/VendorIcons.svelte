40a26e36bd9cc747d9f2d46325183767007fdcd7
b1mind <brent@1mindsolutions.com>
2022-06-08 13:53:54 -0600

    💄border for vender component icons

--- src/lib/component/VendorIcons.svelte
+++ src/lib/component/VendorIcons.svelte
@@ -4,19 +4,28 @@
 
 <div class="icons-wrap">
   {#each techUsed as name}
-    <svg aria-label={name} width="32" height="32">
-      <use href="/img/vendor-icons.svg#{name}" />
-    </svg>
+    <div class="border">
+      <svg aria-label={name} width="32" height="32">
+        <use href="/img/vendor-icons.svg#{name}" />
+      </svg>
+    </div>
   {/each}
 </div>
 
 <style lang="scss">
   .icons-wrap {
-    --fill: var(--clr-primary);
+    // --fill: var(--clr-highlight);
     margin-block-start: var(--spacer-8);
     display: flex;
     gap: var(--gap-3);
     flex-wrap: wrap;
     align-items: center;
+
+    .border {
+      padding: 0.75rem;
+      // border: 2px solid var(--clr-highlight);
+      border: 3px solid var(--clr-secondary-bg);
+      border-radius: 50%;
+    }
   }
 </style>
