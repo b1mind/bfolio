c4fdae21c654f71a2122723e66709aa151abb793
b1mind <brent@1mindsolutions.com>
2023-03-28 11:14:00 -0600

    🧹 linted files

--- src/lib/component/SocialIcons.svelte
+++ src/lib/component/SocialIcons.svelte
@@ -1,22 +1,22 @@
 <script>
-  export let name = 'twitter'
-  export let url = 'twitter.com'
+	export let name = 'twitter'
+	export let url = 'twitter.com'
 
-  let icon = `/img/social-icons.svg#${name}`
+	let icon = `/img/social-icons.svg#${name}`
 </script>
 
 <a href="https://{url}" aria-label={name}>
-  <svg aria-hidden="true" width="24" height="24">
-    <use href={icon} />
-  </svg>
+	<svg aria-hidden="true" width="24" height="24">
+		<use href={icon} />
+	</svg>
 </a>
 
 <style lang="scss">
-  svg {
-    --fill: var(--clr-gray);
-  }
+	svg {
+		--fill: var(--clr-gray);
+	}
 
-  svg:hover {
-    --fill: unset;
-  }
+	svg:hover {
+		--fill: unset;
+	}
 </style>
